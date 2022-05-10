a55c27102cfd891ee6bf92b2865ea923e7d44000
bathtaters <nomoremrnice7@gmail.com>
2022-05-10 04:32:01 -0400

    make modal always center, fix htmlFor props

--- src/components/subcomponents/ModalStyles.js
+++ src/components/subcomponents/ModalStyles.js
@@ -2,8 +2,8 @@
 export const ModalStyle = ({ modalId, hideClose = false, children }) => (<>
   <input type="checkbox" id={modalId} className="modal-toggle" />
 
-  <label for={modalId} className="modal cursor-pointer modal-bottom sm:modal-middle">
-    <label className="modal-box bg-secondary-content text-secondary" for="">
+  <label htmlFor={modalId} className="modal cursor-pointer">
+    <label className="modal-box bg-secondary-content text-secondary" htmlFor="">
       { hideClose || <ModalCloseButton modalId={modalId} /> }
       {children}
     </label>
@@ -11,13 +11,13 @@ export const ModalStyle = ({ modalId, hideClose = false, children }) => (<>
 </>)
 
 export const ModalOpenButton = ({ modalId, children }) => (
-  <label for={modalId} className="btn btn-circle btn-secondary btn-sm sm:btn-md modal-button sm:p-1">
+  <label htmlFor={modalId} className="btn btn-circle btn-secondary btn-sm sm:btn-md modal-button sm:p-1">
     {children}
   </label>
 )
 
 export const ModalCloseButton = ({ modalId }) => (
   <span className="modal-action absolute top-0 right-0 mt-1 mr-1">
-    <label for={modalId} className="btn btn-circle btn-sm btn-ghost hover:bg-secondary hover:text-secondary-content">✕</label>
+    <label htmlFor={modalId} className="btn btn-circle btn-sm btn-ghost hover:bg-secondary hover:text-secondary-content">✕</label>
   </span>
 )
