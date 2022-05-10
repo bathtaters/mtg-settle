bc0346c3e387d0b57037c7848656d47acd4e24e1
bathtaters <nomoremrnice7@gmail.com>
2022-05-10 01:21:40 -0400

    add debug option

--- src/services/dbQuery.services.js
+++ src/services/dbQuery.services.js
@@ -1,6 +1,10 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, useQuery } from "@apollo/client"
-import { databaseParams } from "../assets/constants";
+import { databaseParams } from "../assets/constants"
 
+// DEBUG BLOCK
+import { getDebug } from "../assets/constants"
+import LoadingSpinner from "../components/subcomponents/LoadingSpinner";
+const { cardData, enable: debugging } = getDebug()
 
 // Initialize Apollo client and create provider wrapper
 const client = new ApolloClient({ 
@@ -12,12 +16,12 @@ const client = new ApolloClient({
 export const CardProvider = ({ children }) => <ApolloProvider client={client}>{children}</ApolloProvider>
 
 
-
 // Custom hook for querying GraphQL server
 export function useGetCards(setCode) {
-  const { data, loading, error } = useQuery(databaseParams.query, { variables: { setCode } })
+  const { data, loading, error } = useQuery(databaseParams.query, { variables: { setCode }, skip: debugging }) // DEBUG
 
-  if (loading) return { msg: 'Loading...' }
+  if (debugging) return { data: cardData } // DEBUG
+  if (loading) return { msg: <LoadingSpinner /> }
   if (error) return console.error(error) || { msg: `Error! ${error.message}` }
 
   return { data: data?.sets?.[0]?.cards }
--- src/services/pickSet.controller.js
+++ src/services/pickSet.controller.js
@@ -1,9 +1,13 @@
 import { useMemo } from "react"
 import setList from "../assets/setList.json"
 
+// DEBUG BLOCK
+import { getDebug } from "../assets/constants"
+const { setData, enable: debugging } = getDebug()
+
 const getRandomEntry = (array) => array[Math.floor(Math.random() * array.length)]
 
 export default function usePickSet() {
-  const set = useMemo(() => getRandomEntry(setList), [])
+  const set = useMemo(() => debugging ? setData : getRandomEntry(setList), []) // DEBUG
   return set
 }
