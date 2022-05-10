ba4600633d2ec82092fe266168ef2b96ff807410
bathtaters <nomoremrnice7@gmail.com>
2022-05-10 01:22:59 -0400

    add NavBar

--- src/components/styles/appStyles.js
+++ src/components/styles/appStyles.js
@@ -1,4 +1,12 @@
 
-export const AppWraperStyle = ({ children }) => <div className="max-h-screen max-w-3xl p-4 m-auto">{children}</div>
+export const AppWraperStyle = ({ children }) => (
+  <div className="h-full w-full max-w-3xl max-h-[75rem] min-w-[18rem] min-h-[35rem] p-2 sm:p-4 m-auto overflow-hidden">
+    <div className="flex flex-col justify-between h-full">{children}</div>
+  </div>
+)
 
-export const AppTitleStyle = ({ children }) => <h2 className="text-center py-4">{children}</h2>
+export const AppNavbarStyle = ({ children }) => (<>
+  <div className="navbar justify-center">{children}</div>
+</>)
+
+export const AppTitleStyle = ({ children }) => <h1 className="navbar-center">{children}</h1>
