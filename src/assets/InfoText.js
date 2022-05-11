037f08915bc4d3ccdbbd26b44f9f18cde9421907
bathtaters <nomoremrnice7@gmail.com>
2022-05-11 01:41:12 -0400

    add email link and tweak formatting

--- src/assets/InfoText.js
+++ src/assets/InfoText.js
@@ -3,13 +3,15 @@ import { maxGuessCount } from "./constants"
 
 const gitHubLink = "https://github.com/bathtaters/mtg-settle"
 
+const reportEmail = "bathtaters@gmail.com"
+
 export default function InfoText() {
   return (<>
     <p className="text-scale-lg">
       Try to guess the <i>Magic: the Gathering</i> set based off of the art from {maxGuessCount} cards in a single expansion set.
     </p>
 
-    <div className="text-scale-md my-3 text-secondary/70">
+    <div className="text-scale-md my-4 sm:my-6 text-secondary/70">
       <p>All rights belong to the respective artists</p>
 
       <p>
@@ -24,10 +26,11 @@ export default function InfoText() {
       </p>
     </div>
 
-    <p className="text-scale-xs mt-2 font-sans">
+    <p className="text-scale-xs font-sans">
       <LinkStyle href="https://mtgjson.com">MTGJSON DB</LinkStyle>&nbsp;-&nbsp; 
       <LinkStyle href="https://scryfall.com">Scryfall Art</LinkStyle>&nbsp;-&nbsp;
-      <LinkStyle href={gitHubLink}>GitHub repo</LinkStyle>
+      <LinkStyle href={gitHubLink}>GitHub</LinkStyle>&nbsp;-&nbsp;
+      <LinkStyle href={`mailto:${reportEmail}`}>Email</LinkStyle>
     </p>
   </>)
 }
