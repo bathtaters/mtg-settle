309c54c9d62884c0572e2c616a8efee4f22faab7
bathtaters <nomoremrnice7@gmail.com>
2022-05-19 05:41:14 -0400

    improve Stats styling on mobile

--- src/components/styles/StatsStyles.js
+++ src/components/styles/StatsStyles.js
@@ -4,7 +4,11 @@ import { ShareIcon, NewGameIcon } from "../subcomponents/Icons"
 export const StatsWrapperStyle = ({ children }) => <div className="flex flex-col items-center">{children}</div>
 
 export const InfoWrapperStyle = ({ children }) => (
-  <div className="stats stats-vertical xs:stats-horizontal my-1 text-base font-sans shadow shadow-black bg-secondary-focus text-secondary-content">
+  <div
+    className="stats stats-vertical xs:stats-horizontal shadow shadow-black 
+      bg-secondary-focus text-secondary-content font-sans 
+      mb-1 text-xs sm:text-base"
+  >
     {children}
   </div>
 )
@@ -12,25 +16,29 @@ export const InfoWrapperStyle = ({ children }) => (
 export const InfoItemStyle = ({ title, value, detail }) => (
   <div className="stat">
     <div className="stat-title">{title}</div>
-    <div className="stat-value">{value}</div>
+    <div className="stat-value text-3xl sm:text-4xl">{value}</div>
     <div className="stat-desc">{detail}</div>
   </div>
 )
 
-export const StatsButtonWrapper = ({ children }) => <div className="flex flex-wrap gap-2 justify-center items-center mt-1">{children}</div>
+export const StatsButtonWrapper = ({ children }) => (
+  <div className="flex flex-wrap gap-2 justify-center items-center mt-1">{children}</div>
+)
 
 const StatsButton = ({ onClick, label, children }) => (
   <button type="button" onClick={onClick} className="
-    btn w-full sm:w-auto btn-lg btn-secondary
-    flex flex-wrap-reverse gap-3 sm:gap-4 place-content-start justify-center
+    btn btn-secondary w-full sm:w-auto btn-md sm:btn-lg
+    flex flex-wrap-reverse gap-0 sm:gap-4 place-content-start justify-center
   ">
-    <span className="text-xl sm:text-2xl">{label}</span>
+    <span className="text-lg sm:text-2xl">{label}</span>
     {children}
   </button>
 )
 
 // Stats elements
-export const StatsDivider = () => <div className="divider before:bg-secondary/30 after:bg-secondary/30 w-5/6 self-center" />
+export const StatsDivider = () => (
+  <div className="divider before:bg-secondary/30 after:bg-secondary/30 m-0 sm:m-2 w-5/6 self-center" />
+)
 
 export const ShareButton = ({ onClick }) => (
   <StatsButton onClick={onClick} label="Share">
@@ -47,7 +55,7 @@ export const NewGameButton = ({ onClick }) => (
 // Stats Bar styles
 export const ProgressWrapperStyle = ({ children }) => (
   <div className="
-    grid grid-cols-stats gap-3 p-6 my-2 w-72 max-w-[85vw] text-secondary-content font-sans 
+    grid grid-cols-stats gap-2 p-4 sm:p-6 my-2 w-64 sm:w-72 max-w-[85vw] text-secondary-content font-sans 
     artboard-demo bg-secondary-focus shadow shadow-black
   ">
     {children}
