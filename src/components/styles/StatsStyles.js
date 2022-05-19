1f4e5b02d6492f59e0d70fd5bec2bcb158b8c913
bathtaters <nomoremrnice7@gmail.com>
2022-05-19 06:05:25 -0400

    make stats bigger on desktop

--- src/components/styles/StatsStyles.js
+++ src/components/styles/StatsStyles.js
@@ -7,7 +7,7 @@ export const InfoWrapperStyle = ({ children }) => (
   <div
     className="stats stats-vertical xs:stats-horizontal shadow shadow-black 
       bg-secondary-focus text-secondary-content font-sans 
-      mb-1 text-xs sm:text-base"
+      mb-1 text-xs sm:text-lg"
   >
     {children}
   </div>
@@ -16,7 +16,7 @@ export const InfoWrapperStyle = ({ children }) => (
 export const InfoItemStyle = ({ title, value, detail }) => (
   <div className="stat">
     <div className="stat-title">{title}</div>
-    <div className="stat-value text-3xl sm:text-4xl">{value}</div>
+    <div className="stat-value text-3xl sm:text-5xl">{value}</div>
     <div className="stat-desc">{detail}</div>
   </div>
 )
@@ -42,24 +42,28 @@ export const StatsDivider = () => (
 
 export const ShareButton = ({ onClick }) => (
   <StatsButton onClick={onClick} label="Share">
-    <ShareIcon className="fill-current w-auto h-full py-3" />
+    <ShareIcon className="fill-current w-auto h-full py-2 pl-1 sm:pl-0 sm:py-3" />
   </StatsButton>
 )
 
 export const NewGameButton = ({ onClick }) => (
   <StatsButton onClick={onClick} label="New Game">
-    <NewGameIcon className="fill-current w-auto h-full py-3" />
+    <NewGameIcon className="fill-current w-auto h-full py-2 pl-1 sm:pl-0 sm:py-3" />
   </StatsButton>
 )
 
 // Stats Bar styles
 export const ProgressWrapperStyle = ({ children }) => (
   <div className="
-    grid grid-cols-stats gap-2 p-4 sm:p-6 my-2 w-64 sm:w-72 max-w-[85vw] text-secondary-content font-sans 
+    grid grid-cols-stats gap-2 p-4 sm:p-6 my-2 w-64 sm:w-80 max-w-[85vw] text-secondary-content font-sans 
     artboard-demo bg-secondary-focus shadow shadow-black
   ">
     {children}
   </div>
 )
-export const TooltipStyle = ({ tip, children }) => <div className="tooltip tooltip-secondary self-stretch" data-tip={tip}>{children}</div>
-export const ProgressStyle = (props) => <progress className="progress progress-info border border-info h-full bg-secondary" {...props} />
+export const TooltipStyle = ({ tip, children }) => (
+  <div className="tooltip tooltip-secondary self-stretch" data-tip={tip}>{children}</div>
+)
+export const ProgressStyle = (props) => (
+  <progress className="progress progress-info border border-info h-full bg-secondary" {...props} />
+)
