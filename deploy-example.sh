363e8babe4e6cc584301037fb99c1e46aa515273
Nick C <bathtaters@gmail.com>
2023-03-16 22:21:24 -0400

    add sample deploy script

---
+++ deploy-example.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Path to where this file resides (No trailing slash!)
+repopath="$HOME/path/to/repo/mtg-settle"
+
+# Path to where the server serves built pages (No trailing slash!)
+livepath="/var/www/mtg-settle"
+
+
+
+
+### SCRIPT ###
+
+set -e
+cd "$repopath"
+
+# Update
+echo
+echo " *** UPDATING CLIENT *** "
+git pull
+npm install
+npm update
+
+# Build
+npm run build
+
+# Deploy
+echo
+echo " *** DEPLOYING CLIENT *** "
+if [ -d "$livepath" ]; then
+    mv -f "$livepath" "$livepath-old"
+    echo "> Old client backed up: $livepath-old"
+else
+    echo "> Old client not found"
+fi
+
+cp -a "$repopath/build" "$livepath" && echo "> New client build is live."
