3b629014c84bc6290af087dd58112ffb00c2e0d4
Nick C <bathtaters@gmail.com>
2024-02-13 23:05:33 -0500

    fix sharing issue on Safari for MacOS

--- src/services/subservices/share.services.js
+++ src/services/subservices/share.services.js
@@ -1,14 +1,20 @@
 import { shareDefaults, shareChars } from "../../assets/constants"
 import { blankArray }  from "../app.controller"
 
+// Force clipboard API on MacOS Safari to avoid their frustratingly stupid sharing extension
+const forceClipboard = () => /^((?!chrome|android|ip(hone|pod)|mobile).)*safari/i.test(navigator.userAgent)
+
 async function shareData(text, title = shareDefaults.title, url = shareDefaults.url) {
-  const canShare = Boolean(window.navigator.share)
-  if (canShare) {
-    await window.navigator.share({ title, url, text }).catch(() => {})
+  text = text + '\n' + url
+  if (window.navigator.share && !forceClipboard()) {
+    await window.navigator.share({ title, text }).catch(() => {})
+    return false
   } else if (Boolean(window.navigator.clipboard?.writeText)) {
-    await window.navigator.clipboard.writeText(text + '\n' + url)
-  } else return shareDefaults.failMsg
-  return !canShare && shareDefaults.copyMsg
+    await window.navigator.clipboard.writeText(text)
+    return shareDefaults.copyMsg
+  } else {
+    return shareDefaults.failMsg
+  }
 }
 
 const shareScore = (guesses, guessCount, date, setAlert) => shareData(
